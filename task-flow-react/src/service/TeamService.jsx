348edd28fc7d8f5b8352ebe1866eeee6298e4c1a
Angel Gustavo Sanchez Rodas <asanchezr12@miumg.edu.gt>
2026-01-10 15:58:28 -0600

    Se corrgio una url

--- task-flow-react/src/service/TeamService.jsx
+++ task-flow-react/src/service/TeamService.jsx
@@ -4,7 +4,7 @@ import api from '../api/axiosConfig'; // Asegúrate que la ruta sea correcta
 
 export const salirDelEquipo = async (idEquipo, idUsuario) => {
     try {
-        const response = await api.delete(`/equipos/${idEquipo}/salir/${idUsuario}`);
+        const response = await api.delete(`/equipo/${idEquipo}/salir/${idUsuario}`);
         return response.data;
     } catch (error) {
         console.error("Error al salir del equipo:", error);
